4f7bf75d6431fe7585ab5024f101d4d5f35b2673
ramraghu <raghuramnair2015@gmail.com>
2016-09-26 01:58:14 +0530

    first commit

---
+++ assignment1_3.java
@@ -0,0 +1,18 @@
+package learnJava;
+
+public class assignment1_3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+//Write a program that swaps the values of two variables without using third variable
+
+		int a = 3;
+		int b = 4; 
+		a = a + b ; 
+		b = a - b ; 
+		a = a - b ; 
+		System.out.println(a);
+		System.out.println(b);
+	}
+
+}
